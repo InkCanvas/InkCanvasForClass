226d38b5d6c5f62f24a7673ff918579e9c1a03d4
kriastans <kriastans@protonmail.com>
2024-08-23 13:02:55 +0800

    [update] 新设置UI

--- InkCanvasForClass/Windows/SettingsWindow.xaml.cs
+++ InkCanvasForClass/Windows/SettingsWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Management;
@@ -11,13 +12,17 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Ink_Canvas.Popups;
 using iNKORE.UI.WPF.Helpers;
+using iNKORE.UI.WPF.Modern.Controls;
 using OSVersionExtension;
 
 namespace Ink_Canvas.Windows {
@@ -210,6 +215,11 @@ namespace Ink_Canvas.Windows {
                 if (si.Selected) SettingsWindowTitle.Text = si.Title;
             }
             CollectionViewSource.GetDefaultView(SidebarItems).Refresh();
+
+            AboutPane.Visibility = _selectedSidebarItemName == "AboutItem" ? Visibility.Visible : Visibility.Collapsed;
+            ExtensionsPane.Visibility = _selectedSidebarItemName == "ExtensionsItem" ? Visibility.Visible : Visibility.Collapsed;
+            CanvasAndInkPane.Visibility = _selectedSidebarItemName == "CanvasAndInkItem" ? Visibility.Visible : Visibility.Collapsed;
+            AboutScrollViewerEx.ScrollToTop();
         }
 
         public static class TouchTabletDetectHelper {
@@ -302,6 +312,81 @@ namespace Ink_Canvas.Windows {
                     return null;
                 }
             }
+        }
+
+        private void ScrollViewerEx_ScrollChanged(object sender, ScrollChangedEventArgs e) {
+            var scrollViewer = (ScrollViewer)sender;
+            if (scrollViewer.VerticalOffset >= 10) {
+                DropShadowEffectTopBar.Opacity = 0.25;
+            } else {
+                DropShadowEffectTopBar.Opacity = 0;
+            }
+        }
+
+        private void ScrollBar_Scroll(object sender, RoutedEventArgs e) {
+            var scrollbar = (ScrollBar)sender;
+            var scrollviewer = scrollbar.FindAscendant<ScrollViewer>();
+            if (scrollviewer != null) scrollviewer.ScrollToVerticalOffset(scrollbar.Track.Value);
+        }
+
+        private void ScrollBarTrack_MouseEnter(object sender, MouseEventArgs e) {
+            var border = (Border)sender;
+            if (border.Child is Track track) {
+                track.Width = 16;
+                track.Margin = new Thickness(0, 0, -2, 0);
+                var scrollbar = track.FindAscendant<ScrollBar>();
+                if (scrollbar != null) scrollbar.Width = 16;
+                var grid = track.FindAscendant<Grid>();
+                if (grid.FindDescendantByName("ScrollBarBorderTrackBackground") is Border backgroundBorder) {
+                    backgroundBorder.Width = 8;
+                    backgroundBorder.CornerRadius = new CornerRadius(4);
+                    backgroundBorder.Opacity = 1;
+                }
+                var thumb = track.Thumb.Template.FindName("ScrollbarThumbEx", track.Thumb) ;
+                if (thumb != null) {
+                    var _thumb = thumb as Border;
+                    _thumb.CornerRadius = new CornerRadius(4);
+                    _thumb.Width = 8;
+                    _thumb.Background = new SolidColorBrush(Color.FromRgb(138, 138, 138));
+                }
+            }
+        }
+
+        private void ScrollBarTrack_MouseLeave(object sender, MouseEventArgs e) {
+            var border = (Border)sender;
+            border.Background = new SolidColorBrush(Colors.Transparent);
+            border.CornerRadius = new CornerRadius(0);
+            if (border.Child is Track track) {
+                track.Width = 6;
+                track.Margin = new Thickness(0, 0, 0, 0);
+                var scrollbar = track.FindAscendant<ScrollBar>();
+                if (scrollbar != null) scrollbar.Width = 6;
+                var grid = track.FindAscendant<Grid>();
+                if (grid.FindDescendantByName("ScrollBarBorderTrackBackground") is Border backgroundBorder) {
+                    backgroundBorder.Width = 3;
+                    backgroundBorder.CornerRadius = new CornerRadius(1.5);
+                    backgroundBorder.Opacity = 0;
+                }
+                var thumb = track.Thumb.Template.FindName("ScrollbarThumbEx", track.Thumb) ;
+                if (thumb != null) {
+                    var _thumb = thumb as Border;
+                    _thumb.CornerRadius = new CornerRadius(1.5);
+                    _thumb.Width = 3;
+                    _thumb.Background = new SolidColorBrush(Color.FromRgb(195, 195, 195));
+                }
+            }
+        }
+
+        private void ScrollbarThumb_MouseDown(object sender, MouseButtonEventArgs e) {
+            var thumb = (Thumb)sender;
+            var border = thumb.Template.FindName("ScrollbarThumbEx",thumb);
+            ((Border)border).Background = new SolidColorBrush(Color.FromRgb(95, 95, 95));
+        }
+
+        private void ScrollbarThumb_MouseUp(object sender, MouseButtonEventArgs e) {
+            var thumb = (Thumb)sender;
+            var border = thumb.Template.FindName("ScrollbarThumbEx",thumb);
+            ((Border)border).Background = new SolidColorBrush(Color.FromRgb(138, 138, 138));
         }
 
         private Border _sidebarItemMouseDownBorder = null;
